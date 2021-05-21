c3528c6d35dbe420dd92c4454dfc39802629d0c2
zengqiangfang <zengqiangfang@zengqiangfangdeMacBook-Pro.local>
2021-05-21 10:59:01 +0800

    use authenticaion and authorition

---
+++ JWTAuthentication/JWTAuthentication/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+using Core3.JWT.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core3.JWT.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>();
+            users.Add(new User { Id = 1, Username = "goku", Password = "goku", Role = "manager" });
+            users.Add(new User { Id = 2, Username = "vejeta", Password = "vejeta", Role = "employee" });
+            users.Add(new User { Id = 3, Username = "kuririn", Password = "kuririn", Role = "tester" });
+            return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == x.Password).FirstOrDefault();
+        }
+    }
+}
---
+++ JWTAuthentication/JWTAuthentication/Settings.cs
@@ -0,0 +1,7 @@
+namespace Core3.JWT
+{
+    public static class Settings
+    {       
+         public static string Secret = "marcy9d8534b48w951b9287d492b256x";
+    }
+}
