d0733576175706962bc5a06d054afee237c6ae77
zengqiang.fang <zengqiang.fang@thougthworks.com>
2021-08-23 14:41:47 +0800

    add Action generic function and related unit test

--- FunctionalPractice/MainProgram/FunctionalPro.cs
+++ FunctionalPractice/MainProgram/FunctionalPro.cs
@@ -2,9 +2,9 @@ using System;
 
 namespace MainProgram
 {
-    public class FunctionalPro
+    public static class FunctionalPro
     {
-        public int Count<T>(T[] objectArray, Predicate<T> condition)
+        public static int Count<T>(T[] objectArray, Predicate<T> condition)
         {
             int counter = 0;
             foreach (var obj in objectArray)
@@ -16,6 +16,11 @@ namespace MainProgram
             }
 
             return counter;
+        }
+        
+        public static void Use<T>(this T obj, Action<T> action)
+        {
+            action(obj);
         }
     }
 }
--- FunctionalPractice/MainProgramTest/FunctionalProTests.cs
+++ FunctionalPractice/MainProgramTest/FunctionalProTests.cs
@@ -13,14 +13,25 @@ namespace MainProgramTest
             //Arrange
             Predicate<object> objectIsString = delegate(object obj) { return obj.GetType() == typeof(string); };
             object[] objectArray = {"test", 1, "test", 2, 3, 4};
-            var functionalPro = new FunctionalPro();
 
             //Act
-            var result = functionalPro.Count(objectArray, objectIsString);
+            var result = FunctionalPro.Count(objectArray, objectIsString);
 
             //Assert
             result.Should().NotBe(null);
             result.Should().Be(2);
+        }
+
+        //TODO: how assert this test
+        [Fact]
+        public void GiveUsingBlock_WhenCallUse_ThenExecuteOnce()
+        {
+            //Given
+            Action<string> print = Console.WriteLine;
+            var str= "test";
+
+            //When
+            str.Use(print);
         }
     }
 }
