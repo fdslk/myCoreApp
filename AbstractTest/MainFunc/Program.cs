625f183f11a802c479301c28b9fd2a23e29f3865
zengqiang.fang <zengqiang.fang@thougthworks.com>
2021-09-01 18:19:25 +0800

    add factory practice

--- AbstractTest/MainFunc/Program.cs
+++ AbstractTest/MainFunc/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using AbstractFactoryDesignPatternInCSharp;
+using FactoryDesignPattern;
 
 namespace MainFunc
 {
@@ -7,26 +8,30 @@ namespace MainFunc
     {
         static void Main(string[] args)
         {
-            IMobilePhone nokiaMobilePhone = new Nokia();  
-            MobileFactory nokiaClient = new MobileFactory(nokiaMobilePhone);  
-  
-            Console.WriteLine("********* NOKIA **********");  
-            Console.WriteLine(nokiaClient.GetNormalPhoneDetail());  
-            Console.WriteLine(nokiaClient.GetSamrtPhoneDetail());  
-              
-            IMobilePhone samsungMobilePhone = new Samsung();  
-            MobileFactory samsungClient = new MobileFactory(samsungMobilePhone);  
-  
-            Console.WriteLine("******* SAMSUNG **********");  
-            Console.WriteLine(samsungClient.GetNormalPhoneDetail());  
-            Console.WriteLine(samsungClient.GetSamrtPhoneDetail());  
-            
-            IMobilePhone appleMobilePhone = new Apple();  
-            MobileFactory AppleClient = new MobileFactory(appleMobilePhone);  
-            
-            Console.WriteLine("******* APPLE **********");  
-            Console.WriteLine(AppleClient.GetNormalPhoneDetail());  
-            Console.WriteLine(AppleClient.GetSamrtPhoneDetail());  
+            // IMobilePhone nokiaMobilePhone = new Nokia();  
+            // MobileFactory nokiaClient = new MobileFactory(nokiaMobilePhone);  
+
+            // Console.WriteLine("********* NOKIA **********");  
+            // Console.WriteLine(nokiaClient.GetNormalPhoneDetail());  
+            // Console.WriteLine(nokiaClient.GetSamrtPhoneDetail());  
+
+            // IMobilePhone samsungMobilePhone = new Samsung();  
+            // MobileFactory samsungClient = new MobileFactory(samsungMobilePhone);  
+
+            // Console.WriteLine("******* SAMSUNG **********");  
+            // Console.WriteLine(samsungClient.GetNormalPhoneDetail());  
+            // Console.WriteLine(samsungClient.GetSamrtPhoneDetail());  
+
+            // IMobilePhone appleMobilePhone = new Apple();  
+            // MobileFactory AppleClient = new MobileFactory(appleMobilePhone);  
+
+            // Console.WriteLine("******* APPLE **********");  
+            // Console.WriteLine(AppleClient.GetNormalPhoneDetail());  
+            // Console.WriteLine(AppleClient.GetSamrtPhoneDetail());  
+
+            var dateTimeFactory = DateTimeFactory.GetFactory();
+            var dateTime = dateTimeFactory.ConvertLocalDatetime(20210901);
+            System.Console.WriteLine(dateTime);
   
             Console.ReadKey();          
         }
