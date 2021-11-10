c16c6f8fef231b2f84518bdb6f101d3910e65a9c
zengqiang.fang <zengqiang.fang@thougthworks.com>
2021-11-10 15:19:00 +0800

    add api for upload file
    Signed-off-by: zengqiang.fang <zengqiang.fang@thougthworks.com>

---
+++ uploadDemo/DemoFile/Controllers/FileController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DemoFile.Controller{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController: ControllerBase{
+        private readonly ILogger<FileController> _logger;
+        private readonly IHostingEnvironment _environment;
+
+        public FileController(ILogger<FileController> logger, IHostingEnvironment environment)
+        {
+            _logger = logger;
+            _environment = environment;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
+        {
+            var size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length <= 0) continue;
+
+                await using var stream = System.IO.File.Create(Path.Combine(GetLocalPath(), formFile.FileName));
+                await formFile.CopyToAsync(stream);
+            }
+            
+            return Ok(new { count = files.Count, size });
+        }
+
+        private string GetLocalPath()
+        {
+            var rootPath = _environment.ContentRootPath;
+
+            var path = Path.Combine(rootPath, "Uploads");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            return path;
+        }
+    }
+}
